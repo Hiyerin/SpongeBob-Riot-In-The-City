4d35784802df6af21042c79e9783e309459937f4
Herin <154962851+Hiyerin@users.noreply.github.com>
2024-01-05 14:56:55 +0330

    Add Photon + Add Lobby + Add Room Mananger + Add Plane and character

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    public static RoomManager instance;
+    public GameObject player;
+
+    [Space]
+    public Transform  spawnPoint;
+
+    [Space]
+    public GameObject roomCam;
+    // Start is called before the first frame update
+    
+    void Awake() {
+        instance = this;
+    }
+    void Start()
+    {
+        Debug.Log("Connecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+
+    public override void OnConnectedToMaster() {
+        base.OnConnectedToMaster();
+
+        Debug.Log("Connected To Server!");
+
+        PhotonNetwork.JoinLobby();
+
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+
+        PhotonNetwork.JoinOrCreateRoom("test", null , null);
+
+        Debug.Log("We're Connected and in a room now!");
+
+        Invoke("PhotonInit", 4);
+    }
+
+    void PhotonInit() {
+
+       roomCam.SetActive(false);
+       GameObject _player = PhotonNetwork.Instantiate (player.name, spawnPoint.position,Quaternion.identity);
+
+       _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+
+    }
+
+    public void RespawnPlayer() {
+        GameObject _player = PhotonNetwork.Instantiate (player.name, spawnPoint.position,Quaternion.identity);
+
+        _player.GetComponent<PlayerSetup>().IsLocalPlayer();
+
+    }
+}
